6ff5818241dcd49f5a451613b6369a45c82df21c
OksanaKremendiuk <108355357+OksanaKremendiuk@users.noreply.github.com>
2022-10-29 20:30:35 +0300

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,60 +1,17 @@
-import React, { useState } from "react";
-import axios from "axios";
-
+import React from "react";
+import SearchEngine from "./SearchEngine";
 export default function App() {
-  let [city, setCity] = useState("");
-  let [weather, setWeather] = useState({});
-  let [loaded, setLoaded] = useState(false);
-
-  function showWeather(response) {
-    setLoaded(true);
-    setWeather({
-      cityName: response.data.name,
-      temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
-      humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      description: response.data.weather[0].description,
-    });
-  }
-
-  function handleSearch(event) {
-    event.preventDefault();
-    let api = "4a6d5a2213f3c0c35df9b43a1ead3cfc";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`;
-    axios.get(url).then(showWeather);
-  }
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-  let form = (
-    <form onSubmit={handleSearch}>
-      <input
-        type="search"
-        placeholder="Enter a city..."
-        onChange={updateCity}
-      />
-      <button type="submit">Search</button>
-    </form>
+  return (
+    <div>
+      <SearchEngine />
+      <br />
+      <footer>
+        Coded by Oksana Kremendiuk.{" "}
+        <a href="https://github.com/OksanaKremendiuk/react-app-weather-2">
+          Open source
+        </a>
+        .
+      </footer>
+    </div>
   );
-  if (loaded) {
-    return (
-      <div className="App">
-        {form}
-        <br />
-        <div>Current weather in {weather.cityName}:</div>
-        <ul>
-          <li>Temperature: {Math.round(weather.temperature)}°C</li>
-          <li>Humidity: {weather.humidity}%</li>
-          <li>Wind: {Math.round(weather.wind)} km/h</li>
-          <li>Description: {weather.description}</li>
-          <li>
-            <img src={weather.icon} alt={weather.description} />
-          </li>
-        </ul>
-      </div>
-    );
-  } else {
-    return form;
-  }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -8,23 +9,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-      </header>
-      <App />
-      <br />
-      <footer>
-        Coded by Oksana Kremendiuk.{" "}
-        <a href="https://github.com/OksanaKremendiuk/react-app-weather-2">
-          Open source
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <App />
+      </div>
     </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
